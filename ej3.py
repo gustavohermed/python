d01ec4d857860248c3418c33276ab9ed7bf3f6af
gustavohermed <gustavo.jayena@gmail.com>
2025-09-30 14:12:06 +0200

    ejercicios

---
+++ ej3.py
@@ -0,0 +1,2 @@
+nombre= input("¿Cuál es tu nombre de usuario?")
+print (f"¡Hola {nombre}!")
---
+++ ej6.py
@@ -0,0 +1,4 @@
+horas = float(input("Número de horas trabajadas: "))
+coste = float(input("Coste por hora: "))
+paga = horas * coste
+print("La paga que te corresponde es:", paga)
