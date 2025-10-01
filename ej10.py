993381d78ba0cdd2de6868cc95e577a2b30dcd3e
gustavohermed <gustavo.jayena@gmail.com>
2025-10-01 10:16:06 +0200

    ejercicios

---
+++ ej10.py
@@ -0,0 +1,5 @@
+payasos= int(input("Payasos que quieres pedir: "))
+muñecas= int(input("Muñecas que quieres pedir: "))
+peso= payasos * 112 + muñecas * 75
+
+print (f"El peso del paquete en gramos es: {peso}")
