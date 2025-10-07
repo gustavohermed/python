60cf352bc2279ac4605fa11882b14b8dd1e6fdbe
gustavohermed <gustavo.jayena@gmail.com>
2025-10-07 14:06:57 +0200

    ej2

---
+++ Ejercicios 2./ej2.py
@@ -0,0 +1,9 @@
+
+nombre = input("Dime tu nombre completo: ")
+
+
+print("En minúsculas:", nombre.lower())
+
+print("En mayúsculas:", nombre.upper())
+
+print("Con iniciales en mayúscula:", nombre.title())
