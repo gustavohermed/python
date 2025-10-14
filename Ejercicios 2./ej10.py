600f057e44318f74122b2f4bcf468440c0fa59a1
gustavohermed <gustavo.jayena@gmail.com>
2025-10-14 14:25:00 +0200

    ej10

---
+++ Ejercicios 2./ej10.py
@@ -0,0 +1,7 @@
+compra = input("Dime los productos de tu compra separados por comas: ")
+
+lista_compra = compra.split(",")
+
+print ("Los producto de tu compra son: ")
+for compra in lista_compra:
+    print(compra.split())
