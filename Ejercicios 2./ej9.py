f5dfb69981c2da74933d8478b8aacdcb26411e30
gustavohermed <gustavo.jayena@gmail.com>
2025-10-14 14:09:41 +0200

    ej9

---
+++ Ejercicios 2./ej9.py
@@ -0,0 +1,7 @@
+fecha_nac = input("Escribe tu fecha de nacimiento en este formato dd/mm/aaaa: ")
+
+dia, mes, año = fecha_nac.split ("/")
+
+print (f"Tu dia de cumpleaños es : {dia}, tu mes es: {mes}, y tu año es: {año}")
+
+
