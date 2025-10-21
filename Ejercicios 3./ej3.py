3c0a50bb361cd9510de60d9c2a34885792f770bc
gustavohermed <gustavo.jayena@gmail.com>
2025-10-21 14:06:20 +0200

    ej3

---
+++ Ejercicios 3./ej3.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Introduce el primer numero (dividendo)"))
+numero2 = int(input("Introduce el segundo numero(divisor)"))
+
+if numero2 == 0:
+    print ("Error, no se puede dividir entre 0")
+else:
+    print ("El resultado de la division es:" , numero1/numero2)
