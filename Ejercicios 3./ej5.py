973949354a285a17af764c57a85b487320c5e971
gustavohermed <gustavo.jayena@gmail.com>
2025-10-21 14:17:54 +0200

    ej 5

---
+++ Ejercicios 3./ej5.py
@@ -0,0 +1,7 @@
+edad = int(input("Dime tu edad: "))
+ingresos = float(input("Dime tus ingresos mensuales: "))
+
+if edad > 16 and ingresos >= 1000:
+    print ("Tributas")
+else: 
+    print ("No tributas")
