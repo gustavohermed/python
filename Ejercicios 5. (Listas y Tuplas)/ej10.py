69c3e39a99723f94b1b39bfd44dc7f7dba7f1016
gustavohermed <gustavo.jayena@gmail.com>
2026-01-13 14:43:01 +0100

    ej10

---
+++ Ejercicios 5. (Listas y Tuplas)/ej10.py
@@ -0,0 +1,7 @@
+precios = [50, 75, 46, 22, 80, 65, 8]
+
+precio_minimo = min(precios)
+precio_maximo = max(precios)
+
+print("El precio menor es:", precio_minimo)
+print("El precio mayor es:", precio_maximo)
