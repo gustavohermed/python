818b0e422941e1c4ad4e7b01a389a17b9852e592
gustavohermed <gustavo.jayena@gmail.com>
2026-01-13 14:37:55 +0100

    ej9.py

---
+++ Ejercicios 5. (Listas y Tuplas)/ej9.py
@@ -0,0 +1,16 @@
+palabra = input("Introduce una palabra: ").lower()
+
+vocales = {
+    "a": 0,
+    "e": 0,
+    "i": 0,
+    "o": 0,
+    "u": 0
+}
+
+for letra in palabra:
+    if letra in vocales:
+        vocales[letra] += 1
+
+for vocal, cantidad in vocales.items():
+    print(f"La vocal {vocal} aparece {cantidad} veces")
