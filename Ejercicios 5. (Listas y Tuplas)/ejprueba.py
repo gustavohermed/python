5ac6cad0c520a9e4f1b332616739258825a1fc4c
gustavohermed <gustavo.jayena@gmail.com>
2026-01-13 13:02:50 +0100

    ej1 y repaso

---
+++ Ejercicios 5. (Listas y Tuplas)/ejprueba.py
@@ -0,0 +1,31 @@
+lista = [1,2,3]
+print (lista)
+
+lista.append(4)
+print (lista)
+
+lista.insert(1,100)
+print(lista)
+
+lista.pop()
+print(lista)
+
+lista.pop(1)
+print (lista)
+
+lista.sort(reverse=True)
+print(lista)
+
+print(lista.count(3))
+
+print(lista.index(1))
+
+print(lista[0])
+
+print (max(lista))
+
+print (min(lista))
+
+print (sum(lista))
+
+print (len(lista))
