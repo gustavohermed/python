1a0717ba054ae426d0fe1add0a19b2d66aa52023
gustavohermed <gustavo.jayena@gmail.com>
2026-01-26 19:12:12 +0100

    ej6

---
+++ Ejercicios 6. (Diccionarios)/ej6.py
@@ -0,0 +1,16 @@
+persona = {}
+
+persona["nombre"] = input("Introduce tu nombre: ")
+print(persona)
+
+persona["edad"] = input("Introduce tu edad: ")
+print(persona)
+
+persona["sexo"] = input("Introduce tu sexo: ")
+print(persona)
+
+persona["telefono"] = input("Introduce tu teléfono: ")
+print(persona)
+
+persona["email"] = input("Introduce tu correo electrónico: ")
+print(persona)
