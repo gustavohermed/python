9f3d15757a0fa175affc83878eaeebcc718b3fbc
gustavohermed <gustavo.jayena@gmail.com>
2026-01-26 19:15:21 +0100

    ej7

---
+++ Ejercicios 6. (Diccionarios)/ej7.py
@@ -0,0 +1,19 @@
+cesta = {}
+
+while True:
+    articulo = input("Introduce un artículo (o 'fin' para terminar): ")
+    
+    if articulo == "fin":
+        break
+    
+    precio = float(input(f"Introduce el precio de {articulo}: "))
+    cesta[articulo] = precio
+
+print("\nLista de la compra")
+total = 0
+
+for articulo, precio in cesta.items():
+    print(f"{articulo} {precio}")
+    total += precio
+
+print(f"Total {total}")
